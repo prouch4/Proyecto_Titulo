0083c92f344096e2dfec55c96b3ad73a2b605287
prouch4 <sofia.prochaska@gmail.com>
2025-06-04 14:10:53 -0400

    Update

--- python/graficos/prueba.py
+++
@@ -1,5 +0,0 @@
-import sys
- #calculate stuff
-sys.stdout.write('Bugs: 5 |Other: 10\n')
-sys.stdout.flush()
-sys.exit(0)
--- python/ticket/__init__.py
+++
@@ -1,3 +0,0 @@
-from python.ticket import connection_db
-from python.ticket import correction_for_db
-from python.ticket import get_fresh_service
